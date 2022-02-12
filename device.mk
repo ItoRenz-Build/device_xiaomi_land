afbecf2eec594bbc140aa0143938ff7380d90562
blazerpaul <blazerpaul15@gmail.com>
2022-02-12 21:26:31 +0800

    land: Add prebuilt Graphene Camera
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+PRODUCT_PACKAGES += \
+    GrapheneCamera
+
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
